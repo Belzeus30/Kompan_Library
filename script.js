4dc377c4ba0b5c7b6808f34430ed3cfe447bd50b
Belzeus30 <radekraisigl@gmail.com>
2023-09-08 10:05:05 +0200

    asfsafsafsafsdafsa

--- script.js
+++ script.js
@@ -43,8 +43,25 @@ const books = document.querySelectorAll(".book");
 // Get a reference to the modal content
 const modalContent = document.getElementById("modalContent");
 
+// Function to open the modal
+function openModal() {
+    modal.style.display = "block";
+}
+
+// Function to close the modal
+function closeModalFunction() {
+    modal.style.display = "none";
+    // Clear the cloned book content
+    modalContent.innerHTML = "";
+}
+
+// Function to close the cloned book within the modal
+function closeClonedBook() {
+    closeModalFunction();
+}
+
 // Attach click event listeners to each book
-books.forEach(book => {
+books.forEach((book) => {
     book.addEventListener("click", function () {
         // Clone the clicked book square
         const clonedBook = this.cloneNode(true);
@@ -55,6 +72,12 @@ books.forEach(book => {
         // Set the class of the cloned book to "enlarged-book" for styling
         clonedBook.classList.add("enlarged-book");
 
+        // Remove the "More info" paragraph from the cloned book
+        const moreInfo = clonedBook.querySelector(".book-footer");
+        if (moreInfo) {
+            moreInfo.remove();
+        }
+
         // Remove transition and transform properties
         clonedBook.style.transition = "none";
         clonedBook.style.transform = "none";
@@ -63,21 +86,68 @@ books.forEach(book => {
         modalContent.innerHTML = "";
         modalContent.appendChild(clonedBook);
 
-        // Display the modal
-        modal.style.display = "block";
+
+
+        // Open the modal
+        openModal();
     });
 });
 
 // Close the modal when the close button is clicked
-closeModal.addEventListener("click", function () {
-    modal.style.display = "none";
-});
+closeModal.addEventListener("click", closeModalFunction);
 
-// Close the modal when the user clicks outside of it
+// Close the cloned book within the modal when the user clicks outside of it
 window.addEventListener("click", function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target === modalContent) {
+        closeModalFunction();
     }
 });
 
 
+// Get all the star elements
+///////
+var stars = document.querySelectorAll('.star');
+
+// Add a click event listener to each star
+stars.forEach(function(star) {
+    star.addEventListener('click', function() {
+        var rating = this.getAttribute('data-rating'); // Get the rating value
+
+        // Set the rating for this book
+        var bookDetails = this.closest('.book').querySelector('.book-details');
+        bookDetails.querySelector('p:nth-child(4)').textContent = 'Rating: ' + '★'.repeat(rating) + '☆'.repeat(5 - rating);
+
+        // Set the rating value as a data attribute for the book
+        bookDetails.querySelector('.rating').setAttribute('data-rating', rating);
+
+        // Update the rating for the other stars
+        stars.forEach(function(s) {
+            var sRating = s.getAttribute('data-rating');
+            s.textContent = sRating <= rating ? '★' : '☆';
+        });
+    });
+});
+function filterFunction() {
+    // Get the search input value
+    var input = document.querySelector('.search');
+
+
+    // Get all the book elements within the bookgrid
+    var books = document.querySelectorAll('.bookgrid .book');
+
+    // Loop through all the book elements and hide/show them based on the search input
+    for (var i = 0; i < books.length; i++) {
+        var book = books[i];
+        var title = book.querySelector('h3');
+        var author = book.querySelector('p:nth-child(2)'); // Assuming author is the second <p> element
+        var titleText = title.textContent || title.innerText;
+        var authorText = author.textContent || author.innerText;
+
+        // If the book title or author name contains the search text, display the book; otherwise, hide it
+        if (titleText.toUpperCase().indexOf(filter) > -1 || authorText.toUpperCase().indexOf(filter) > -1) {
+            book.style.display = '';
+        } else {
+            book.style.display = 'none';
+        }
+    }
+}
